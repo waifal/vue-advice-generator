d03d860aa56d8072be3ba3874ed99edf5d11afdf
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-06-02 11:22:55 +1200

    Reformat

--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -1,81 +1,81 @@
 <template>
     <nav>
         <strong>Vue Advice Generator</strong>
-        <button type="button" @click="toggleTheme" @mouseover="showTooltip"><i class="fa-solid fa-moon"></i>
-            <Tooltip text="Toggle Theme"/>
+        <button type="button" @click="toggleTheme" @mouseover="showTooltip">
+            <i class="fa-solid fa-moon"></i>
+            <Tooltip text="Toggle Theme" />
         </button>
     </nav>
-    <hr>
+    <hr />
 </template>
 
 <script>
-    import Tooltip from './Tooltip.vue';
+import Tooltip from "./Tooltip.vue";
 
-    export default {
-        emits: ["toggle-theme"], 
-        components: { Tooltip },
-        methods: {
-            toggleTheme() {
-                this.$emit("toggle-theme");
-            }
-        }
-    };
+export default {
+    emits: ["toggle-theme"],
+    components: { Tooltip },
+    methods: {
+        toggleTheme() {
+            this.$emit("toggle-theme");
+        },
+    },
+};
 </script>
 
 <style scoped>
-    nav {
-        height: 80px;
-        padding: 0 4rem;
-        display: flex;
-        align-items: center;
+nav {
+    height: 80px;
+    padding: 0 4rem;
+    display: flex;
+    align-items: center;
+}
+
+nav > * {
+    color: #2f2f2f;
+}
+
+nav > strong {
+    color: #e4e4e4;
+}
+
+nav + hr {
+    height: 1px;
+    border: none;
+    background: linear-gradient(to right, transparent, #171717, transparent);
+}
+
+button {
+    width: 45px;
+    height: 45px;
+    margin-left: auto;
+    font-size: 1.2rem;
+    border: none;
+    border-radius: 50%;
+    background: none;
+    position: relative;
+    transition: 0.2s linear background, 0.2s linear color;
+}
+
+button:hover {
+    color: #575757;
+    background: rgba(46, 46, 46, 0.1);
+}
+
+button:hover > div {
+    display: block;
+    animation: tooltip-fade 0.3s ease-in-out;
+}
+
+@keyframes tooltip-fade {
+    from {
+        opacity: 0;
+        top: 160%;
     }
 
-    nav > * {
-        color: #2f2f2f;
+    to {
+        opacity: 1;
+        top: 155%;
     }
-
-    nav > strong {
-        color: #e4e4e4;
-    }
-
-    nav + hr {
-        height: 1px;
-        border: none;
-        background: linear-gradient(to right, transparent, #171717, transparent);
-    }
-
-    button {
-        width: 45px;
-        height: 45px;
-        margin-left: auto;
-        font-size: 1.2rem;
-        border: none;
-        border-radius: 50%;
-        background: none;
-        position: relative;
-        transition: 0.2s linear background, 0.2s linear color;
-    }
-
-    button:hover {
-        color: #575757;
-        background: rgba(46, 46, 46, 0.1);
-    }
-
-    button:hover > div {
-        display: block;
-        animation: tooltip-fade 0.3s ease-in-out;
-    }
-
-    @keyframes tooltip-fade {
-        from {
-            opacity: 0;
-            top: 160%;
-        }
-
-        to {
-            opacity: 1;
-            top: 155%
-        }
-    }
-
+}
 </style>
--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -3,38 +3,38 @@
 </template>
 
 <script>
-    export default {
-        props: ['text'],
-    }
+export default {
+    props: ["text"],
+};
 </script>
 
 <style scoped>
-    div {
-        display: none;
-        width: fit-content;
-        height: 35px;
-        padding: 0 0.8rem;
-        color: #fff;
-        align-content: center;
-        font-size: 0.9rem;
-        position: absolute;
-        left: 50%;
-        top: 155%;
-        white-space: nowrap;
-        transform: translate(-50%, -50%);
-        border-radius: 8px;
-        background-color: rgba(46, 46, 46, 0.5);
-        transition: opacity 0.2s linear;
-    }
+div {
+    display: none;
+    width: fit-content;
+    height: 35px;
+    padding: 0 0.8rem;
+    color: #fff;
+    align-content: center;
+    font-size: 0.9rem;
+    position: absolute;
+    left: 50%;
+    top: 155%;
+    white-space: nowrap;
+    transform: translate(-50%, -50%);
+    border-radius: 8px;
+    background-color: rgba(46, 46, 46, 0.5);
+    transition: opacity 0.2s linear;
+}
 
-    div::before {
-        content: "";
-        position: absolute;
-        left: 50%;
-        top: -33%;
-        transform: translateX(-50%);
-        border-width: 6px; 
-        border-style: solid;
-        border-color:  transparent transparent rgba(46, 46, 46, 0.5) transparent;
-    }
+div::before {
+    content: "";
+    position: absolute;
+    left: 50%;
+    top: -33%;
+    transform: translateX(-50%);
+    border-width: 6px;
+    border-style: solid;
+    border-color: transparent transparent rgba(46, 46, 46, 0.5) transparent;
+}
 </style>
